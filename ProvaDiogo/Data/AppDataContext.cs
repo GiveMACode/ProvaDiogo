4e3321818bb2defc16ae1a1fcf970c3463bcec39
Vinicius Atanasio <vinicius_atanasio@hotmail.com>
2023-10-09 19:31:08 -0300

    DataContext | ef framework

---
+++ ProvaDiogo/Data/AppDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProvaDiogo.Models;
+
+namespace ProvaDiogo.Data;
+
+public class AppDataContext : DbContext
+{
+    public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+    {
+    }
+
+    //public DbSet<Usuario> Usuario { get; set; }
+
+}
