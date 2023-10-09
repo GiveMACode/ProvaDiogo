6c1e0031cede9c6d239cfa09129a1e8db6c67dcf
Vinicius Atanasio <vinicius_atanasio@hotmail.com>
2023-10-09 20:10:00 -0300

    Models

---
+++ ProvaDiogo/Models/Folha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ProvaDiogo.Models;
+
+public class Folha
+{
+    public int FolhaId { get; set; }
+
+    public int Valor { get; set; }
+
+    public int Quantidade { get; set; }
+
+    public int Mes { get; set; }
+    
+    public int Ano { get; set; }
+}
---
+++ ProvaDiogo/Models/Funcionario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ProvaDiogo.Models;
+
+public class Funcionario
+{
+    public int FuncionarioId { get; set; }
+
+    public String Nome { get; set; }
+
+    public String Cpf {get; set; }
+
+}
